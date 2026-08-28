30d5661e279a9d1e304b86c7194f3037bddb17e2
shivraj598 <timilsenashivraj598@gmail.com>
2026-08-28 22:15:52 +0545

    feat: enhance RAM widget with graph representation and improved styling

--- sketchybar/.config/sketchybar/items/widgets/ram.lua
+++ sketchybar/.config/sketchybar/items/widgets/ram.lua
@@ -5,8 +5,15 @@ local settings = require("settings")
 -- Execute the native event provider which reports used RAM every 2.0 seconds.
 sbar.exec("killall ram_load >/dev/null; $CONFIG_DIR/helpers/event_providers/ram_load/bin/ram_load ram_update 2.0")
 
-local ram = sbar.add("item", "widgets.ram", {
+local ram = sbar.add("graph", "widgets.ram", 42, {
   position = "right",
+  graph = { color = colors.blue },
+  background = {
+    height = 22,
+    color = { alpha = 0 },
+    border_color = { alpha = 0 },
+    drawing = true,
+  },
   icon = { string = icons.ram },
   label = {
     string = "ram ??%",
@@ -15,13 +22,17 @@ local ram = sbar.add("item", "widgets.ram", {
       style = settings.font.style_map["Bold"],
       size = 9.0,
     },
+    align = "right",
     padding_right = 0,
+    width = 0,
     y_offset = 4,
   },
   padding_right = settings.paddings + 6,
 })
 
 ram:subscribe("ram_update", function(env)
+  local usage = tonumber(env.ram_usage)
+  ram:push({ usage / 100. })
   ram:set({ label = "ram " .. env.ram_usage .. "%" })
 end)
 
