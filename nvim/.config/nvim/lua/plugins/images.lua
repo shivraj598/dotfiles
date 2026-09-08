e503bdf1c7de0dda7d587c5bf61d6e680f604233
shivraj598 <timilsenashivraj598@gmail.com>
2026-09-08 23:52:46 +0545

    add: create configuration for snacks.nvim plugin with image options

---
+++ nvim/.config/nvim/lua/plugins/images.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      image = {
+        enabled = true,
+        doc = {
+          enabled = true,
+          inline = false,
+          float = true,
+          max_width = 60,
+          max_height = 30,
+          conceal = false,
+        },
+      },
+    },
+  },
+}
