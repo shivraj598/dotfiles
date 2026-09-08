a3809b2a8f326e5f06eda45f44b4f8a59ac49e2b
shivraj598 <timilsenashivraj598@gmail.com>
2026-09-08 23:00:34 +0545

    add: create catppuccin plugin configuration for LazyVim

---
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "LazyVim/LazyVim",
+    opts = { colorscheme = "catppuccin" },
+  },
+  {
+    "catppuccin",
+    opts = {
+      transparent = true,
+      flavor = "mocha",
+    },
+  },
+}
