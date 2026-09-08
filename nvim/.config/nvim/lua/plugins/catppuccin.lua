802de0f3cfb86689982e7381ce8fd6164e18b3dd
shivraj598 <timilsenashivraj598@gmail.com>
2026-09-08 23:52:41 +0545

    add: implement auto command for catppuccin color scheme and update custom highlights

--- nvim/.config/nvim/lua/plugins/catppuccin.lua
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -5,10 +5,48 @@ return {
   },
   {
     "catppuccin",
+    init = function()
+      vim.api.nvim_create_autocmd("ColorScheme", {
+        pattern = "catppuccin",
+        callback = function()
+          for _, group in ipairs({
+            "Normal",
+            "NormalNC",
+            "NormalFloat",
+            "FloatBorder",
+            "SignColumn",
+            "EndOfBuffer",
+            "StatusLine",
+            "StatusLineNC",
+            "SnacksDashboardNormal",
+            "SnacksDashboardHeader",
+            "SnacksDashboardDesc",
+            "SnacksDashboardIcon",
+            "SnacksDashboardKey",
+            "SnacksDashboardFooter",
+            "SnacksDashboardDir",
+          }) do
+            vim.api.nvim_set_hl(0, group, { bg = "none" })
+          end
+        end,
+      })
+    end,
     opts = {
       transparent_background = true,
       flavor = "mocha",
-      float = { transparent = true },
+      float = { transparent = true, solid = false },
+      custom_highlights = function(colors)
+        return {
+          Normal = { bg = colors.none },
+          NormalNC = { bg = colors.none },
+          NormalFloat = { bg = colors.none },
+          FloatBorder = { bg = colors.none },
+          SignColumn = { bg = colors.none },
+          EndOfBuffer = { bg = colors.none },
+          StatusLine = { bg = colors.none },
+          StatusLineNC = { bg = colors.none },
+        }
+      end,
     },
   },
 }
