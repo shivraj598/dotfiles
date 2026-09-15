9537297fe4241c67265ac780e2b31eefcd4ceb33
shivraj598 <timilsenashivraj598@gmail.com>
2026-09-15 06:59:53 +0545

    feat: add keymap to return to Normal mode using 'jk' in Insert mode

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Press jk in Insert mode to return to Normal mode.
+vim.keymap.set("i", "jk", "<Esc>", { noremap = true, silent = true })
