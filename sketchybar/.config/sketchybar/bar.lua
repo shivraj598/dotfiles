efdf0d11ce68c901e4fda3aabd96737d4d07b2f0
shivraj598 <timilsenashivraj598@gmail.com>
2026-09-17 22:15:59 +0545

    fix: adjust bar dimensions for improved layout

--- sketchybar/.config/sketchybar/bar.lua
+++ sketchybar/.config/sketchybar/bar.lua
@@ -2,8 +2,8 @@ local colors = require("colors")
 
 -- Equivalent to the --bar domain
 sbar.bar({
-  height = 35,
+  height = 33,
   color = colors.bar.bg,
-  padding_right = 2,
-  padding_left = 2,
+  padding_right = 5,
+  padding_left = 5,
 })
